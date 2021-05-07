dc8074678f4d50ae824dbcac7ce2fec40469e399
unknown <121498367@qq.com>
2021-05-07 10:39:52 +0800

    0507

--- src/main/java/com/snow/MyValve.java
+++ src/main/java/com/snow/MyValve.java
@@ -18,12 +18,15 @@ public class MyValve extends ValveBase {
     @Override
     public void invoke(Request request, Response response) throws IOException, ServletException {
         HttpServletResponse httpResponse = response.getResponse();
-        httpResponse.setHeader("Content-Security-Policy", "default-src 'self' 'unsafe-inline' *.gacycu.cn *.eol.cn *.alicdn.com;" +
-                "script-src 'self' 'unsafe-inline' 'unsafe-eval' *.baidu.com  *.bdimg.com *.eol.cn;" +
-                "script-src-elem 'self' 'unsafe-inline' 'unsafe-eval' *.baidu.com  *.bdimg.com *.eol.cn;" +
-                "img-src 'self' *.baidu.com *.bdimg.com *.eol.cn *.gacycu.cn data: blob:;" +
-                "font-src 'self' data: *.alicdn.com *.gacycu.cn;" +
-                "frame-ancestors 'self';");
+        httpResponse.setHeader("Content-Security-Policy", "default-src 'self' 'unsafe-inline' *.jxsycm.com *.qq.com;" +
+                "script-src 'self' 'unsafe-inline' 'unsafe-eval' *.baidu.com *.baomitu.com *.qq.com;" +
+                "script-src-elem 'self' 'unsafe-inline' 'unsafe-eval' *.baidu.com *.alicdn.com *.baomitu.com *.qq.com;" +
+                "style-src-elem 'self' 'unsafe-inline' 'unsafe-eval' *.alicdn.com *.baomitu.com;" +
+                "img-src 'self' *.baidu.com *.jxsycm.com data: blob: *.qq.com qzonestyle.gtimg.cn qzapp.qlogo.cn;" +
+                "font-src 'self' data: *.jxsycm.com *.alicdn.com *.baomitu.com;" +
+                "frame-src 'self' *.jxsycm.com *.qq.com;" +
+                "frame-ancestors 'self' *.jxsycm.com *.qq.com;");
+        httpResponse.setHeader("Access-Control-Allow-Origin","*");
         getNext().invoke(request, response);
     }
 }
